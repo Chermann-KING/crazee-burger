de2572d9db96f9a5c0e2a06aa79b47ea282ab41d
Chermann-KING <codes.smiling@gmail.com>
2023-01-18 15:07:00 +0100

    ✨ F02: App> Add navigation by routing to OrderPage, LoginPage, ErrorPage

---
+++ src/components/pages/order/OrderPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function OrderPage() {
+  // State (état, données)
+
+  // Comportement (fonctions)
+
+  // Affichage (JSX)
+  return (
+    <div>
+      <h1>Bonjour !</h1>
+      <Link to="/">
+        <button>Déconnexion</button>
+      </Link>
+    </div>
+  );
+}
