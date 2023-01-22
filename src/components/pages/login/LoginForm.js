48a4b6cdb4b5b361b53475ab6bfe1a664ab87ea4
Chermann-KING <codes.smiling@gmail.com>
2023-01-22 13:54:53 +0100

    💄 F03: Login > Styling LoginPage and LoginForm

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { theme } from "../../../theme/theme";
+import { BsPersonCircle } from "react-icons/bs";
+import { IoChevronForward } from "react-icons/io5";
 
 export default function LoginForm() {
   // state (état, données)
@@ -21,20 +25,110 @@ export default function LoginForm() {
 
   // affichage (JSX)
   return (
-    <form action="submit" onSubmit={handleSubmit}>
-      <h1>Bienvenue chez nous !</h1>
-      <br />
-      <h2>Connectez-vous</h2>
-      <input
-        onChange={handleChange}
-        value={inputText}
-        type="text"
-        placeholder="Entrez votre prénom..."
-        required
-      />
-      <button type="submit">Accédez à votre espace</button>
-    </form>
+    <LoginFormStyled action="submit" onSubmit={handleSubmit}>
+      <div className="header-form">
+        <h2>Bienvenue chez nous !</h2>
+        <hr />
+        <h3>Connectez-vous</h3>
+      </div>
+
+      <div className="cta-form">
+        <div className="input-wrapper">
+          <BsPersonCircle className="icon" />
+          <input
+            onChange={handleChange}
+            value={inputText}
+            type="text"
+            placeholder="Entrez votre prénom"
+            required
+          />
+        </div>
+
+        <button type="submit">
+          Accéder à mon espace <IoChevronForward />
+        </button>
+      </div>
+    </LoginFormStyled>
   );
 }
 
+const LoginFormStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
 
+  gap: ${theme.spacing.md};
+
+  .header-form h2 {
+    font-size: ${theme.fonts.P5};
+  }
+  .header-form h3 {
+    font-size: ${theme.fonts.P4};
+  }
+  hr {
+    border: 2.5px solid ${theme.colors.primary_burger};
+    border-radius: ${theme.borderRadius.subtle};
+    margin: ${theme.spacing.md} 0;
+  }
+
+  .cta-form {
+    display: flex;
+    flex-direction: column;
+
+    gap: ${theme.spacing.sm};
+
+    .input-wrapper {
+      display: flex;
+      align-items: center;
+      gap: ${theme.spacing.md};
+      padding: ${theme.spacing.md} ${theme.spacing.xl};
+      border-radius: ${theme.borderRadius.round};
+      background-color: ${theme.colors.white};
+
+      .icon {
+        color: ${theme.colors.greySemiDark};
+      }
+      input[type="text"] {
+        background-color: transparent;
+        font-size: ${theme.fonts.P0};
+        border-radius: ${theme.borderRadius.round};
+        border: 0.5px solid ${theme.colors.white};
+        -webkit-transition: 0.3s;
+        transition: 0.3s;
+        outline: none;
+        width: 100%;
+
+        ::placeholder {
+          color: ${theme.colors.greyMedium};
+        }
+        :focus {
+          border: 0.5px solid ${theme.colors.greyBlue};
+        }
+      }
+    }
+
+    button {
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: ${theme.spacing.md};
+      border-radius: ${theme.borderRadius.round};
+      background-color: ${theme.colors.primary_burger};
+      padding: ${theme.spacing.md} 0;
+      width: 100%;
+      color: ${theme.colors.white};
+      font-weight: ${theme.weights.bold};
+      letter-spacing: 0.1rem;
+      :hover {
+        background-color: ${theme.colors.white};
+        padding: 19px 0;
+        color: ${theme.colors.primary_burger};
+        border: 0.5px solid ${theme.colors.primary_burger};
+        -webkit-transition: 0.1s;
+        transition: 0.1;
+      }
+    }
+  }
+`;
--- src/components/pages/login/LoginPage.js
+++ src/components/pages/login/LoginPage.js
@@ -1,6 +1,67 @@
 import React from "react";
 import LoginForm from "./LoginForm";
+import styled from "styled-components";
+import imageBG from "../../../assets/images/burger-background.jpg";
+import logo from "../../../assets/images/logo-orange.png";
+import { theme } from "../../../theme/theme";
+import { Link } from "react-router-dom";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <LoginPageStyled>
+      <header className="header">
+        <Link to="/">
+          <h1>
+            Crazee
+            <img src={logo} alt="" aria-hidden="true" />
+            Burger
+          </h1>
+        </Link>
+      </header>
+
+      <LoginForm />
+    </LoginPageStyled>
+  );
 }
+
+const LoginPageStyled = styled.div`
+  background-image: url(${imageBG});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+
+  cursor: default;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+  width: 100%;
+
+  gap: ${theme.spacing.xxl};
+
+  .header {
+    width: min-content;
+
+    h1 {
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      font-family: "Amatic SC", cursive;
+      font-size: clamp(68px, 8vw, 6rem);
+      color: ${theme.colors.primary_burger};
+
+      img {
+        width: 14vw;
+        min-width: 120px;
+        height: auto;
+      }
+    }
+  }
+  form {
+    font-family: "Amatic SC", cursive;
+    color: white;
+  }
+`;
