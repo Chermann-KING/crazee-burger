09a8dbba4826633398de3752515162dc5223c338
Chermann-KING <codes.smiling@gmail.com>
2023-01-31 21:00:32 +0100

    💄 F04: OrderPage> Creating the <Main/>

---
+++ src/components/pages/order/Main.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Main() {
+  return <MainStyled></MainStyled>;
+}
+const MainStyled = styled.main`
+  height: 88vh;
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  box-shadow: 0px 0px 30px -3px rgba(0, 0, 0, 0.71) inset;
+  -webkit-box-shadow: 0px 0px 30px -3px rgba(0, 0, 0, 0.71) inset;
+  -moz-box-shadow: 0px 0px 30px -3px rgba(0, 0, 0, 0.71) inset;
+
+  h5 {
+    text-align: center;
+  }
+`;
