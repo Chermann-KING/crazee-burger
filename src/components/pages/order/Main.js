c789a3ace6fb650943045bc9b334e93c70597bd5
Chermann-KING <codes.smiling@gmail.com>
2023-02-01 12:57:27 +0100

    💄 F04: OrderPage> Main> Adding border raduis to the bottom of <Main/>

--- src/components/pages/order/Main.js
+++ src/components/pages/order/Main.js
@@ -14,6 +14,8 @@ const MainStyled = styled.main`
   align-items: center;
   justify-content: center;
 
+  border-radius: 0 0 15px 15px;
+
   box-shadow: 0px 0px 30px -3px rgba(0, 0, 0, 0.71) inset;
   -webkit-box-shadow: 0px 0px 30px -3px rgba(0, 0, 0, 0.71) inset;
   -moz-box-shadow: 0px 0px 30px -3px rgba(0, 0, 0, 0.71) inset;
