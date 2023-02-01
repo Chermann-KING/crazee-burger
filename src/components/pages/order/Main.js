08a3319d9b77efb213a66f9826ac8abdbae2ff15
Chermann-KING <codes.smiling@gmail.com>
2023-02-01 13:16:45 +0100

    💄 F04: OrderPage> Main> Changing the value of the box-shadow

--- src/components/pages/order/Main.js
+++ src/components/pages/order/Main.js
@@ -16,9 +16,9 @@ const MainStyled = styled.main`
 
   border-radius: 0 0 15px 15px;
 
-  box-shadow: 0px 0px 30px -3px rgba(0, 0, 0, 0.71) inset;
-  -webkit-box-shadow: 0px 0px 30px -3px rgba(0, 0, 0, 0.71) inset;
-  -moz-box-shadow: 0px 0px 30px -3px rgba(0, 0, 0, 0.71) inset;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  -webkit-box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  -moz-box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
   h5 {
     text-align: center;
