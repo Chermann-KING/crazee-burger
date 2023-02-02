fcb44b79b80406ee0a4a8de5545a836a96a669e6
Chermann-KING <codes.smiling@gmail.com>
2023-02-02 23:21:46 +0100

    💄 F04: OrderPage> Addaptation of border-radus to <Navbar/> and <Main/> with respect to the system design

--- src/components/pages/order/Main.js
+++ src/components/pages/order/Main.js
@@ -1,11 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import { theme } from "../../../theme/theme";
 
 export default function Main() {
   return <MainStyled></MainStyled>;
 }
 const MainStyled = styled.main`
-  background-color: #f5f5f7;
+  background-color: ${theme.colors.background_white};
   /* overflow-y: scroll; */
   height: 85vh;
   max-width: 1400px;
@@ -16,8 +17,8 @@ const MainStyled = styled.main`
   align-items: center;
   justify-content: center;
 
-  border-radius: 0 0 15px 15px;
-
+  border-radius: 0 0 ${theme.borderRadius.extraRound}
+    ${theme.borderRadius.extraRound};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   -webkit-box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   -moz-box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
