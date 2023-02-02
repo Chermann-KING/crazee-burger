40b81126be65266eb8bb9ff7f8561aab67511705
Chermann-KING <codes.smiling@gmail.com>
2023-02-02 22:22:08 +0100

    ♻️ F04: OrderPage> Navbar> Creation of <UserConnected/>

--- src/components/hoc/Navbar.js
+++ src/components/hoc/Navbar.js
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import { Link, useParams } from "react-router-dom";
-import { BsPersonCircle } from "react-icons/bs";
+import { useParams } from "react-router-dom";
 import Logo from "../hoc/Logo";
 import { theme } from "../../theme/theme";
+import UserConnected from "./UserConnected";
 
 export default function Navbar() {
   // State (état, données)
@@ -18,19 +18,7 @@ export default function Navbar() {
   return (
     <NavbarStyled>
       <Logo />
-
-      <div className="user">
-        <div className="is-connected">
-          <p>
-            Hey, <span>{userName}</span>
-          </p>
-          <Link to="/">
-            <button>Se déconnecter</button>
-          </Link>
-        </div>
-
-        <BsPersonCircle className="icon" />
-      </div>
+      <UserConnected userName={userName} />
     </NavbarStyled>
   );
 }
@@ -49,38 +37,4 @@ const NavbarStyled = styled.nav`
   align-items: center;
   padding: 0 ${theme.spacing.xl};
   border-radius: 15px 15px 0 0;
-
-  .user {
-    display: flex;
-    align-items: center;
-    gap: ${theme.spacing.sm};
-
-    .is-connected {
-      color: ${theme.colors.greyDark};
-      text-align: right;
-      p > span {
-        color: ${theme.colors.primary_burger};
-        font-weight: ${theme.weights.bold};
-      }
-      button {
-        cursor: pointer;
-        color: ${theme.colors.greyDark};
-        background-color: transparent;
-        border: none;
-        margin-top: ${theme.spacing.xxs};
-        padding: ${theme.spacing.xxs} 0;
-        border-bottom: 1px solid transparent;
-
-        transition: all 0.2s ease;
-        :hover {
-          border-bottom: 1px solid ${theme.colors.greyDark};
-        }
-      }
-    }
-    .icon {
-      width: 36px;
-      height: auto;
-      color: ${theme.colors.greyDark};
-    }
-  }
 `;
---
+++ src/components/hoc/UserConnected.js
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+import { theme } from "../../theme/theme";
+import { Link } from "react-router-dom";
+import { BsPersonCircle } from "react-icons/bs";
+
+export default function UserConnected({ userName }) {
+  return (
+    <UserConnectedStyled>
+      <div className="is-connected">
+        <p>
+          Hey, <span>{userName}</span>
+        </p>
+        <Link to="/">
+          <button>Se déconnecter</button>
+        </Link>
+      </div>
+
+      <BsPersonCircle className="icon" />
+    </UserConnectedStyled>
+  );
+}
+
+const UserConnectedStyled = styled.div`
+  display: flex;
+  align-items: center;
+  gap: ${theme.spacing.sm};
+
+  .is-connected {
+    color: ${theme.colors.greyDark};
+    text-align: right;
+    p > span {
+      color: ${theme.colors.primary_burger};
+      font-weight: ${theme.weights.bold};
+    }
+    button {
+      cursor: pointer;
+      color: ${theme.colors.greyDark};
+      background-color: transparent;
+      border: none;
+      margin-top: ${theme.spacing.xxs};
+      padding: ${theme.spacing.xxs} 0;
+      border-bottom: 1px solid transparent;
+
+      transition: all 0.2s ease;
+      :hover {
+        border-bottom: 1px solid ${theme.colors.greyDark};
+      }
+    }
+  }
+  .icon {
+    width: 36px;
+    height: auto;
+    color: ${theme.colors.greyDark};
+  }
+`;
