93d9e54a3ad8c00aa8820cc4411fe6f82a812950
Chermann-KING <codes.smiling@gmail.com>
2023-02-03 14:06:18 +0100

    ✨ F04: OrderPage> Navbar> Make a generic click for the <Logo/>

--- src/components/hoc/Logo.js
+++ src/components/hoc/Logo.js
@@ -4,44 +4,31 @@ import styled from "styled-components";
 import { theme } from "../../theme/theme";
 import picto from "../../assets/images/logo-orange.webp";
 
-export default function Logo({ className }) {
-  const reload = () => {
-    window.location.reload();
-  };
-
+export default function Logo({ className, onClick }) {
   return (
-    <LogoStyled>
-      {/* <Link to="/"> */}
-      <h1 className={className} onClick={reload}>
-        Crazee
-        <img src={picto} alt="" aria-hidden="true" />
-        Burger
-      </h1>
-      {/* </Link> */}
+    <LogoStyled className={className} onClick={onClick}>
+      Crazee
+      <img src={picto} alt="" aria-hidden="true" />
+      Burger
     </LogoStyled>
   );
 }
 
-const LogoStyled = styled.div`
+const LogoStyled = styled.h1`
   z-index: 1;
   width: min-content;
-  cursor: pointer;
   padding: ${theme.spacing.sm} 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-  h1 {
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  font-family: "Amatic SC", cursive;
+  font-size: calc(${theme.fonts.P6}*.5);
+  color: ${theme.colors.primary_burger};
 
-    font-family: "Amatic SC", cursive;
-    font-size: calc(${theme.fonts.P6}*.5);
-    color: ${theme.colors.primary_burger};
-
-    img {
-      /* width: 239px; */
-      width: 60px;
-      height: auto;
-      margin: 0 0.2rem;
-    }
+  img {
+    width: 60px;
+    height: auto;
+    margin: 0 0.2rem;
   }
 `;
