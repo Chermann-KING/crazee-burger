164b008acbe1ee17b8b3cf698cc9b21afd8cf4ee
Chermann-KING <codes.smiling@gmail.com>
2023-02-10 15:00:29 +0100

    💄 F05: OrderPage> Main> Card> Reloading unresized images and changing the Display from Flex to Grid

--- src/components/hoc/Card.js
+++ src/components/hoc/Card.js
@@ -10,7 +10,9 @@ export default function Card({ title, imageSource, price }) {
       <div className="img-card">
         <img src={imageSource} alt={title} />
       </div>
+
       <h2>{title}</h2>
+
       <div className="cta-card">
         <h3>{formatPrice(price)}</h3>
         <Button label={"Ajouter"} />
@@ -20,11 +22,19 @@ export default function Card({ title, imageSource, price }) {
 }
 
 const CardStyled = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-rows: 65% 1fr;
+  padding: 20px;
+  padding-bottom: 10px;
+
+  /* display: flex;
   justify-content: flex-end;
   flex-direction: column;
   padding: 20px 20px;
   gap: 20px;
+  width: 240px;
+  height: 330px; */
+
   width: 240px;
   height: 330px;
   background-color: white;
@@ -32,15 +42,22 @@ const CardStyled = styled.div`
   box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
 
   .img-card {
-    display: flex;
+    /* display: flex;
     justify-content: center;
     width: 200px;
-    height: 145px;
+    height: 145px; */
+    width: 100%;
+    height: auto;
+    margin-top: 30px;
+    margin-bottom: 20px;
 
     img {
-      max-width: 100%;
+      /* max-width: 100%;
       max-height: 100%;
-      align-self: flex-end;
+      align-self: flex-end; */
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
     }
   }
   h2 {
@@ -50,6 +67,7 @@ const CardStyled = styled.div`
     overflow: hidden;
     font-family: "Amatic SC", cursive;
     font-size: 36px;
+    margin-top: -10px;
   }
 
   .cta-card {
