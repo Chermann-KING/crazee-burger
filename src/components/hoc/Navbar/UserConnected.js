dd150192cebc648f6c927b49d54c0f49ec19c2a0
Chermann-KING <codes.smiling@gmail.com>
2023-02-16 14:14:00 +0100

    💄 F06: OrderPage> Navbar> ToggleButton> Restyled toast

--- src/components/hoc/Navbar/UserConnected.js
+++ src/components/hoc/Navbar/UserConnected.js
@@ -39,7 +39,7 @@ export default function UserConnected({ userName }) {
         onToggle={displayAdminToast}
       />
       {/* <button>Notify</button>; */}
-      <ToastContainer />
+      <ToastContainer className="toaster" bodyClassName="body-toast" />
       <div className="is-connected">
         <p>
           Hey, <span>{userName}</span>
@@ -88,5 +88,24 @@ const UserConnectedStyled = styled.div`
     width: 36px;
     height: auto;
     color: ${theme.colors.greyDark};
+  }
+
+  /* TOAST RESTYLED */
+  .toaster {
+    max-width: 300px;
+  }
+
+  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
+    background: ${theme.colors.background_dark};
+  }
+
+  .body-toast {
+    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
+      margin-right: 20px;
+      margin-left: 5px;
+    }
+    div {
+      line-height: 1.3em;
+    }
   }
 `;
